fde2180286770b961b3168011bed2c071e8ce4cb
Erik Visser <erik.aad.visser@gmail.com>
2019-06-08 15:08:32 +0200

    Add FE release 1

---
+++ backend/src/main/resources/static/precache-manifest.fb04dcd5cfe5eda872d50507a34f69be.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a12aa7663a491ef52cbae6b7ee791cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f35a678fa780ec9bcb23",
+    "url": "/static/js/2.1ee705a5.chunk.js"
+  },
+  {
+    "revision": "f5cabf8c05c7d94b2b53",
+    "url": "/static/js/main.e1b44ca1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
