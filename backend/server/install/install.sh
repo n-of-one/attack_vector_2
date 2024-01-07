7b6f73f5a9fd306450aa62f3450c458ea22b8870
Erik Visser <Erik.Aad.Visser@gmail.com>
2024-01-07 14:43:43 +0100

    Server scripts for running on an Ubuntu server -allow ssh, and reduce mongodb memory usage

--- backend/server/install/install.sh
+++ backend/server/install/install.sh
@@ -16,6 +16,10 @@ curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyri
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+# reduce memory usage of MongoDB
+sudo cp mongod.conf /etc/mongod.confls
+
 sudo systemctl start mongod
 sleep 2
 # sudo systemctl status mongod
@@ -28,12 +32,13 @@ sudo apt install -y openjdk-17-jre-headless
 sudo apt install -y maven
 
 # Port forwarding 80 -> 8080
-#sudo ufw allow 8080/tcp
-#
-#sudo sed -i '1iCOMMIT' /etc/ufw/before.rules
-#sudo sed -i '1i-A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080' /etc/ufw/before.rules
-#sudo sed -i '1i:PREROUTING ACCEPT [0:0]' /etc/ufw/before.rules
-#sudo sed -i '1i*nat' /etc/ufw/before.rules
+sudo ufw allow ssh
+sudo ufw allow 8080/tcp
+
+sudo sed -i '1iCOMMIT' /etc/ufw/before.rules
+sudo sed -i '1i-A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080' /etc/ufw/before.rules
+sudo sed -i '1i:PREROUTING ACCEPT [0:0]' /etc/ufw/before.rules
+sudo sed -i '1i*nat' /etc/ufw/before.rules
 
 echo y | sudo ufw enable
 
