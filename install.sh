8c2a5a68caff05624bab62a6b7f0a2c9441d83f5
Erik Visser <Erik.Aad.Visser@gmail.com>
2024-01-14 22:59:07 +0100

    running attack vector on server from root directory.

--- install.sh
+++ install.sh
@@ -1,2 +1,48 @@
+# Install script
+#
+# To run this script, first manually clone Attack Vector 2 repo to the server:
+#
+# git clone https://github.com/n-of-one/attack_vector_2.git
+#
+# Then run this script:
+#
+# cd attack_vector_2/backend/server/install
+# chmod 770 install.sh
+# ./install.sh
+#
+
+# install Mongodb, instructions from: https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# reduce memory usage of MongoDB
+sudo cp install/mongod.conf /etc/mongod.confls
+
+sudo systemctl start mongod
+sleep 2
+# sudo systemctl status mongod
+
+# Create MongoDB user
+mongosh -file install/createUser.js
+
+# Install Java & Maven
+sudo apt install -y openjdk-17-jre-headless
+sudo apt install -y maven
+
+# Make server scripts executable
+chmod 770 run.sh
+chmod 770 setenv.sh
+chmod 770 upgrade.sh
+
+# Allow Java to bind to port 80
+sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/lib/jvm/java-17-openjdk-arm64/bin/java
+
+# Compile
 cd backend
-mvn install -DskipTests
+mvn package -DskipTests
+
+# Run
+cd ..
+./run.sh
