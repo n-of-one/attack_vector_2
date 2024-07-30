d29b9753c0df1754221e663bab898eb1fd3ff99e
Erik <erik.aad.visser@gmail.com>
2024-07-30 13:14:22 +0200

    Add stash/unstash to upgrade scripts to allow local changes.

--- install/upgrade.bat
+++ install/upgrade.bat
@@ -2,7 +2,10 @@
 taskkill /F /IM java.exe
 call setenv.bat
 cd attack_vector_2
+git stash
 git pull
+git stash apply
+git stash clear
 cd backend
 call %MVN_HOME%\bin\mvn clean install -DskipTests
 cd ..\..
--- install/upgrade.sh
+++ install/upgrade.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 killall java
 cd attack_vector_2
+git stash
 git pull
+git stash apply
+git stash clear
 cd backend
 mvn clean install -DskipTests
 cd ../..
