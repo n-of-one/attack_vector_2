1a7acea259f16d8d32f36f61a36aa249f120178f
Erik Visser <erik.aad.visser@gmail.com>
2025-03-26 13:14:19 +0100

    Fix incompatible index name

--- backend/src/main/kotlin/org/n1/av2/layer/ice/password/IcePasswordStatusEntityAndRepo.kt
+++ backend/src/main/kotlin/org/n1/av2/layer/ice/password/IcePasswordStatusEntityAndRepo.kt
@@ -12,7 +12,7 @@ import java.time.ZonedDateTime
 @Document
 data class IcePasswordStatus(
     @Id val id: String, // iceId
-    @Indexed(unique = true) val layerId: String,
+    @Indexed(unique = true, name = "layerId_1") val layerId: String,
     val hackerAttempts: List<String>,  // only attempts by hackers are tracked
     val attemptCount: Int = 0,         // but all attempts count for the purpose of locking
     val lockedUntil: ZonedDateTime,
