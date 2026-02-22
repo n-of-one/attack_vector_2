fb2c5345398f3649ae77bd0283469839151fddeb
Erik Visser <erik.aad.visser@gmail.com>
2026-02-22 12:50:16 +0100

    Set syntax Highlighting after disconnect correctly.

--- backend/src/main/kotlin/org/n1/av2/run/terminal/inside/CommandDisconnectService.kt
+++ backend/src/main/kotlin/org/n1/av2/run/terminal/inside/CommandDisconnectService.kt
@@ -2,17 +2,20 @@ package org.n1.av2.run.terminal.inside
 
 import org.n1.av2.hacker.hackerstate.HackerStateRunning
 import org.n1.av2.run.RunService
+import org.n1.av2.run.terminal.SyntaxHighlightingService
 import org.springframework.stereotype.Service
 
 @Service
 class CommandDisconnectService(
     private val runService: RunService,
     private val insideTerminalHelper: InsideTerminalHelper,
+    private val syntaxHighlightingService: SyntaxHighlightingService,
 ) {
 
     fun disconnect(hackerState: HackerStateRunning) {
         if (!insideTerminalHelper.verifyInside(hackerState)) return
 
         runService.hackerDisconnect(hackerState.toState(), "Disconnected")
+        syntaxHighlightingService.sendForOutside()
     }
 }
