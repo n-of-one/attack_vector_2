99a66f5d121139720b46d0ee62bd4d3293809ac0
Erik Visser <erik.aad.visser@gmail.com>
2026-03-02 19:59:03 +0100

    Add e2e test for script effect: rotate ICE

--- test/e2e/4.1-script-effect-automatically-hack-specific-ICE-layer.spec.ts
+++ test/e2e/4.1-script-effect-automatically-hack-specific-ICE-layer.spec.ts
@@ -48,7 +48,7 @@ test('4.1.1 - Create script and give to hacker', async ({page}: { page: Page }) 
 
     await test.step("Give script to hacker", async () => {
         await scriptManagement.navigateToCurrentScriptsOf("hacker")
-        await scriptManagement.addScriptAndLoad(SCRIPT_NAME, 3)
+        await scriptManagement.addScriptAndLoad(SCRIPT_NAME, 1)
     })
 })
 
@@ -81,26 +81,4 @@ test('4.1.2 - Script works on intended ICE', async ({page}: { page: Page }) => {
         await hacker.verifyScreenshotCanvas("node-hacked")
     })
 
-
 })
-
-// test('4.1.3 - Script does not work on other ICE', async ({page}: { page: Page }) => {
-//     test.setTimeout(15_000);
-//
-//     const [login, hacker] = [new LoginPage(page), new HackerPage(page)]
-//
-//     await startHack(login, hacker)
-//
-//     await test.step("Run script and verify effect", async () => {
-//         await hacker.verifyScreenshotCanvas("node-protected")
-//
-//         await hacker.typeCommand("qmove 04")
-//
-//         const scriptCode = await hacker.getCodeForScript(SCRIPT_NAME)
-//         await hacker.typeCommand(`run ${scriptCode} 1`)
-//
-//         await hacker.waitForNodeHackedAnimationFinished()
-//         await hacker.expectTerminalText("ICE hack complete.")
-//         await hacker.verifyScreenshotCanvas("node-hacked")
-//     })
-// })
