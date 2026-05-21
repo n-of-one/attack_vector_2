dcacc358c1cc4c2324dafb007d860950eccbbb35
Théo <theo.henry@orange.fr>
2026-05-21 14:51:53 +0200

    exporting docker config to raspberry pi

--- deploy/docker_with_keycloak/README_export.md
+++ deploy/docker_with_keycloak/README_export.md
@@ -1,6 +1,9 @@
-# Export only the docker configuration to run it on another device
+# Export only the docker configuration to run on a raspberry pi (linux/arm64)
 
 1. Edit the .env file follow the README.md directives
 2. Run `sh export.sh` and wait
 3. Copy the newly created folder export to the other device
 4. Run `bash run.sh` inside the export folder
+
+## Load a new version of the av2 image
+`docker load -i av2image.tar`
--- deploy/docker_with_keycloak/export.sh
+++ deploy/docker_with_keycloak/export.sh
@@ -6,10 +6,12 @@ mkdir export
 echo 'Copy env and conf'
 cp ./.env ./export
 cp -R ./conf/ ./export
+mkdir ./export/conf/mongoDB
+cp -R ../../install/createUser.js ./export/conf/mongoDB
 
 echo 'Clean cache and build a new docker image of AV2'
 echo 'This step can be long and take more than 10 minutes'
-docker build --no-cache ../../. -t av2:latest
+docker build --no-cache ../../. -t av2:latest --platform linux/arm64
 
 echo 'Export the docker image as tar file'
 docker save -o ./export/av2image.tar av2:latest
@@ -18,12 +20,16 @@ echo 'Copy and modify the compose.yaml file to run outside of the repo'
 cp ./compose.yaml ./export
 cp ../../compose.yaml ./export/av2only_compose.yaml
 
-line_old='file: ../../compose.yaml'
-line_new='file: ./av2only_compose.yaml'
-sed -i "s%$line_old%$line_new%g" ./export/compose.yaml
+line_old1='file: ../../compose.yaml'
+line_new1='file: ./av2only_compose.yaml'
+sed -i "s%$line_old1%$line_new1%g" ./export/compose.yaml
 
 sed -i "s/build:/image: av2:latest/g" ./export/av2only_compose.yaml
 sed -i "s/context: .//g" ./export/av2only_compose.yaml
+
+line_old2='./install/createUser.js'
+line_new2='./conf/mongoDB/createUser.js'
+sed -i "s%$line_old2%$line_new2%g" ./export/av2only_compose.yaml
 
 echo 'Creating a run.sh file'
 echo "#!/bin/bash
